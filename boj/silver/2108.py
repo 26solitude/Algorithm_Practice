c9496d01976a31593b0d40684ea5377afd41c55a
hojang <26dev@naver.com>
2025-06-19 01:12:40 +0900

    solved.ac 2단계 끝

--- boj/silver/2108.py
+++ boj/silver/2108.py
@@ -1,15 +1,27 @@
+import sys
 from collections import Counter
+
+input = sys.stdin.readline
 
 n = int(input())
 arr = []
 for _ in range(n):
     arr.append(int(input()))
 
-a = sum(arr) // n
-arr.sort()
-b = arr[n//2]
-counter = Counter(arr)
-c = counter.most_common()
-d = max(arr) - min(arr)
+result = []
+result.append(str(round(sum(arr) / n)))
 
-print(a, b, c, d)
+arr.sort()
+result.append(str(arr[n // 2]))
+
+cnt = Counter(arr)
+max_v = max(cnt.values())
+max_arr = [k for k, v in cnt.items() if v == max_v]
+if len(max_arr) == 1:
+    result.append(str(max_arr[0]))
+else:
+    result.append(str(max_arr[1]))
+
+result.append(str(max(arr) - min(arr)))
+
+print("\n".join(result))
